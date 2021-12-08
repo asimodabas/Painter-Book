06d0002d5e612b004627a231d63a556951bd83e5
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-08 18:57:56 +0300

    design started

---
+++ app/src/main/java/com/asimodabas/painter_book/DetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.asimodabas.painter_book
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
