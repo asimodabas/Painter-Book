fecd26f05bac41162f16b2c89dad965bf0e844c9
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-09 20:41:41 +0300

    recycler row,view binding added and design +

--- app/src/main/java/com/asimodabas/painter_book/DetailsActivity.kt
+++ app/src/main/java/com/asimodabas/painter_book/DetailsActivity.kt
@@ -2,10 +2,19 @@ package com.asimodabas.painter_book
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.painter_book.databinding.ActivityDetailsBinding
+import com.asimodabas.painter_book.databinding.ActivityMainBinding
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
 }
--- app/src/main/java/com/asimodabas/painter_book/MainActivity.kt
+++ app/src/main/java/com/asimodabas/painter_book/MainActivity.kt
@@ -2,10 +2,28 @@ package com.asimodabas.painter_book
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.painter_book.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
+    fun saveButtonClicked(){
+
+
+    }
+
+    fun selectImage(){
+
+
+    }
+
 }
