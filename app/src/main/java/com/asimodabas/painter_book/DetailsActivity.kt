f028301a319dee5fc5738ee5f3e7149ae9928281
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-10 22:12:21 +0300

    page update and fun created

--- app/src/main/java/com/asimodabas/painter_book/DetailsActivity.kt
+++ app/src/main/java/com/asimodabas/painter_book/DetailsActivity.kt
@@ -1,9 +1,17 @@
 package com.asimodabas.painter_book
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.asimodabas.painter_book.databinding.ActivityDetailsBinding
-import com.asimodabas.painter_book.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -15,6 +23,48 @@ class DetailsActivity : AppCompatActivity() {
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.button.setOnClickListener {
+            saveButtonClicked()
+        }
+
+        binding.imageView.setOnClickListener {
+            selectImage()
+        }
+
+
+    }
+
+    fun saveButtonClicked() {
+
+
+    }
+
+    fun selectImage() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                Snackbar.make(binding.imageView,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",View.OnClickListener {
+
+                //request permission
+
+                }).show()
+
+            }else{
+
+                //request permission
+
+            }
+
+        }else{
+
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                  //intent
+        }
+
     }
 
 }
--- app/src/main/java/com/asimodabas/painter_book/MainActivity.kt
+++ app/src/main/java/com/asimodabas/painter_book/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.asimodabas.painter_book
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.asimodabas.painter_book.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -38,14 +46,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun saveButtonClicked() {
 
-
-    }
-
-    fun selectImage() {
-
-
-    }
 
 }
